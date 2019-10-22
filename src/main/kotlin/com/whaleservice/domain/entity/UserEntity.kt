550c2d2f0ea51c8f436edc40b783260ed5ffc553
kazuma <kazumazuma1124@gmail.com>
2019-10-22 10:58:01 +0900

    add meil validation

--- src/main/kotlin/com/whaleservice/domain/entity/UserEntity.kt
+++ src/main/kotlin/com/whaleservice/domain/entity/UserEntity.kt
@@ -1,6 +1,7 @@
 package com.whaleservice.domain.entity
 import org.springframework.beans.factory.annotation.Autowired
 import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.Pattern
 import javax.validation.constraints.Size
 
 data class UserEntity(
@@ -15,5 +16,6 @@ data class UserEntity(
         var password: String? = "default" ,
         @get:Size(max = 20)
         @get:NotEmpty
+        @Pattern(regexp = "^([\\w])+([\\w\\._-])*\\@([\\w])+([\\w\\._-])*\\.([a-zA-Z])+$")
         var email: String? = "default"
 )

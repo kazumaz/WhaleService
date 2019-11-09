7aff0520b90b96155f0ed7a4839e18fd8fb23b05
kazuma <kazumazuma1124@gmail.com>
2019-11-09 16:01:51 +0900

    add logstash

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("net.logstash.logback:logstash-logback-encoder:6.1")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 
 

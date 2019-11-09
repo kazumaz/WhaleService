dfe7c0b0e393d003dfac9fe706f7a1e83cf90976
kazuma <kazumazuma1124@gmail.com>
2019-11-09 18:12:29 +0900

    add logger

--- src/main/kotlin/com/whaleservice/logger.kt
+++ src/main/kotlin/com/whaleservice/logger.kt
@@ -1,29 +1,23 @@
-//package com.whaleservice
-//
-//import com.library.logmessage.LogMessage
-//import org.slf4j.LoggerFactory.*
-//import org.slf4j.Logger
-//import org.springframework.beans.factory.BeanCreationException
-//import org.springframework.beans.factory.InjectionPoint
-//import org.springframework.context.annotation.Bean
-//import org.springframework.context.annotation.Configuration
-//import org.springframework.context.annotation.Scope
-//
-//enum class ScoreHubLog(
-//        override val messageId: String,
-//        override val message: String
-//): LogMessage {
-//    TESTMSG1("TESTMSGID1", "This is my Msg Test1: "),
-//    TESTMSG2("TESTMSGID2", "This is my Msg Test2: ")
-//}
-//
-//@Configuration
-//open class LoggerInjectionConfig {
-//    @Bean
-//    @Scope("prototype")
-//    open fun logger(ip: InjectionPoint): Logger {
-//        return getLogger(ip.methodParameter?.containingClass
-//                ?: ip.field?.declaringClass
-//                ?: throw BeanCreationException("Cannot find type for Logger"))
-//    }
-//}
+package com.whaleservice
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.BeanCreationException
+import org.springframework.beans.factory.InjectionPoint
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Scope
+
+@Configuration
+class LoggerInjectionConfig {
+//    see[https://saiya-moebius.hatenablog.com/entry/2017/11/08/033932]
+    @Bean
+    @Scope("prototype")
+    fun logger(ip : InjectionPoint): Logger {
+        return LoggerFactory.getLogger(
+                ip.methodParameter?.containingClass
+                        ?: ip.field?.declaringClass
+                        ?: throw BeanCreationException("Cannot find type for Logger")
+        )
+    }
+}

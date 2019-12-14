0336d9099f4f3ff168c79335f1ec2c612d3532f5
kazuma <kazumazuma1124@gmail.com>
2019-12-14 18:25:45 +0900

    grafana. prometheus追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,8 +24,8 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("net.logstash.logback:logstash-logback-encoder:6.1")
+	implementation("io.micrometer:micrometer-registry-prometheus")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-
 
 
 	compile("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
